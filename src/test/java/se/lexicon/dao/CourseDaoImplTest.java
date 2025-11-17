fccd2834739ea4634c938f8d331bbc9a97d4b733
Iffat-Sudo <zabin.ifa@gmail.com>
2025-11-17 11:42:55 +0100

    Added the changes

---
+++ src/test/java/se/lexicon/dao/CourseDaoImplTest.java
@@ -0,0 +1,37 @@
+package se.lexicon.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseDaoImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findByDate() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
